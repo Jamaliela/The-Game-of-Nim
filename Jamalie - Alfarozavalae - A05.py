52307613b76157b785062ee62a647ed26a50a869
jamaliela <Jamalie@berea.edu>
2018-10-01 23:49:29 -0400

    Updated

--- Jamalie - Alfarozavalae - A05.py
+++ Jamalie - Alfarozavalae - A05.py
@@ -11,38 +11,56 @@
 # Acknowledgements:
 #
 #
-####################################################################################
+###############################################################################
 
 
 def getting_started():
     """
-    Boolean function takes an integer as input. Returns True if even and False if odd
 
-
+    :return:
     """
 
     global num_balls
+    global limit
     num_balls = int(input("how many balls do you want to start with?"))
     limit = 15
     while num_balls < limit:
         print("Please enter a number equal or greater than 15")
     print("you are starting with", num_balls, "balls")
+    return num_balls
 
 
 def player_1_turn():
+    """
+
+    :return:
+    """
+
     global player1
-    player1 =int(input("How many balls do you wanna take? Please choose from 1 to 4:"))
+    global num_balls
+    global turn
+    player1 = int(input("How many balls do you wanna take? Please choose from 1 to 4:"))
     limit1 = 4
+    turn = 1
     while player1 > limit1:
         print("Not an option, try again")
-    return print("you took", player1, "balls")
+    num_balls = num_balls - player1
+    print("you took", player1, "balls")
+    print("there are", num_balls, "left")
+    return num_balls, turn
 
 
 def player_2_turn():
-    global player2
-    player2 = num_balls - player1
-    player2 = player2 % 5
-    return print("The computer takes", player2, "balls")
+    global num_balls
+    global turn
+    turn = 0
+    player2 = num_balls % 5
+    if player2 == 0:
+        player2 = 1
+    num_balls = num_balls - player2
+    print("The computer takes", player2, "balls")
+    print("there are", num_balls, "left")
+    return num_balls, turn
 
 
 def main():
@@ -53,12 +71,14 @@ def main():
 
 getting_started()
 
-limit = 0
+limit = 1
 while num_balls > limit:
     player_1_turn()
     player_2_turn()
-    num_balls = num_balls - player2
+if turn == "0":
+    print("the computer wins")
+else:
+    print("You win")
 
-print("Game over")
 
 main()
