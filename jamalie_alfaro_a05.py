654f662a66cffd68ccebaf78cd4e2250b215f542
jamaliela <Jamalie@berea.edu>
2018-10-02 21:05:11 -0400

    almost done

--- jamalie_alfaro_a05.py
+++ jamalie_alfaro_a05.py
@@ -16,51 +16,41 @@
 
 def getting_started():
     """
-
+    This function asks the user to input the number of balls in order to start the game
     :return:
     """
 
-    global num_balls
-    global limit
-    num_balls = int(input("how many balls do you want to start with?"))
-    limit = 15
-    while num_balls < limit:
-        print("Please enter a number equal or greater than 15")
+    num_balls = int(input("how many balls do you want to start with?"))                     # asks the user to enter number of balls
+    while num_balls < 15:                                                                   # while function to check if the input is less than 15
+        num_balls = int(input("Please enter a number equal or greater than 15"))            # make the user enter the number of balls again
     print("you are starting with", num_balls, "balls")
     return num_balls
 
 
-def player_1_turn():
+def player_1_turn(num_balls):
     """
 
     :return:
     """
 
-    global player1
-    global num_balls
-    global turn
     player1 = int(input("How many balls do you wanna take? Please choose from 1 to 4:"))
-    limit1 = 4
-    turn = 1
-    while player1 > limit1:
-        print("Not an option, try again")
+    while (player1 < 1) and (player1 > 4):
+        player1 = int(input("Not an option, try again"))
     num_balls = num_balls - player1
     print("you took", player1, "balls")
     print("there are", num_balls, "left")
-    return num_balls, turn
+    return num_balls
 
 
-def player_2_turn():
-    global num_balls
-    global turn
-    turn = 0
+def player_2_turn(num_balls):
+
     player2 = num_balls % 5
     if player2 == 0:
         player2 = 1
     num_balls = num_balls - player2
     print("The computer takes", player2, "balls")
     print("there are", num_balls, "left")
-    return num_balls, turn
+    return num_balls
 
 
 def main():
@@ -68,17 +58,21 @@ def main():
 
     """
 
+    turn = 0
+    num_balls = getting_started()
+    while num_balls > 0:
+        num_balls = player_1_turn(num_balls)
+        turn = 1
+        if num_balls == 0:
+            break
+        num_balls = player_2_turn(num_balls)
+        turn = 0
+    if turn == "0":
+        print("the computer wins")
+    else:
+        print("You win")
 
-getting_started()
 
-limit = 1
-while num_balls > limit:
-    player_1_turn()
-    player_2_turn()
-if turn == "0":
-    print("the computer wins")
-else:
-    print("You win")
+if __name__ == "__main__":
 
-
-main()
+    main()
